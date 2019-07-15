4ca87441d363b0da55ef55ff95e0e1723c56dbdb
Olly <U201412236@hust.edu.cn>
2019-07-15 20:17:13 +0800

    modify nn input: 5channels to 3channels

--- file_reader/raw_reader.py
+++ file_reader/raw_reader.py
@@ -6,18 +6,25 @@ def raw_reader(path, i):
     """
     :param path: path to mhd file
             i: the id of slice in raw file
-    :return: a 5 channel numpy array
+    :return: a 3 channel numpy array
     """
     # file_name = tool_packages.get_filename(path)
     img_set, origin, spacing = tool_packages.raw_image_reader(path)
     masked_img = []
-    if i-2 < 0 or i+3 >img_set.shape[0]:
-        return None
-    for j in range(i - 2, i + 3, 1):
-        image = np.squeeze(img_set[j, ...])
-        masked_img.append(image)
+    if i == 0:
+        masked_img.append(img_set[0])
+        masked_img.append(img_set[0])
+        masked_img.append(img_set[1])
+    elif i == img_set.shape[0]-1:
+        masked_img.append(img_set[-2])
+        masked_img.append(img_set[-1])
+        masked_img.append(img_set[-1])
+    else:
+        for j in range(i - 1, i + 2, 1):
+            image = np.squeeze(img_set[j, ...])
+            masked_img.append(image)
 
-    five_channels = np.array(masked_img).transpose((1, 2, 0))
+    three_channels = np.array(masked_img).transpose((1, 2, 0))
 
-    return five_channels
+    return three_channels
 
